7616cd09baafed2f6aefc72325933d1b2ec92f44
mulxone <thomasbkeeran@gmail.com>
2026-05-04 02:41:42 -0500

    first update

--- visabound-ai/app/dashboard/page.tsx
+++ visabound-ai/app/dashboard/page.tsx
@@ -2,9 +2,8 @@ export default function Dashboard() {
     return (
       <div className="p-10">
         <h1 className="text-2xl font-bold">
-          Welcome to VisaBound.ai
+          Welcome to VisaBound.ai Thomas
         </h1>
       </div>
     );
   }
-  
